c4e5230222d955945bedba379f925d9d97e15a4c
Olcik-resh <olechkaresetova@gmail.com>
2024-11-29 16:05:08 +0300

    task1

--- script.js
+++ script.js
@@ -1,71 +1,7 @@
-//task1
-
-let a = '10';
-alert(a);
-a = 20;
-alert(a);
-
-//task2
-
-let year = 2007;
-alert(year);
-
-//task3
-
-let a = 'Брендан Эйх';
-alert(a);
-
-//task4
-
-let a = 10;
-let b = 2;
-
-alert(a + b);
-alert(a - b);
-alert(a * b);
-alert(a / b);
-
-//task5
-
-let result = 2 ** 5;
-alert(result);
-
-//task6
-
-let a = 9;
-let b = 2;
-
-alert(a % b);
-
-//task7
-
-let num = 1;
-num += 5; 
-num -= 3;
-num *= 7; 
-num /= 3; 
-++num;    
---num;    
-alert(num);
-
-//task8
-
-let age = prompt("Сколько вам лет?"); 
-alert(age); 
-
-//task9
-
-let user = {
-    name: 'Ольга',
-    age: 34,
-    isAdmin: true
-  };
-  
-  alert(user.name); 
-  alert(user.age); 
-  alert(user.isAdmin); 
-
- //task10
-
-  let userName = prompt("Как вас зовут?");
-  alert(`Привет, ${userName}!`);
+let password = 'пароль';
+let userPassword = prompt("Введите пароль");
+if (userPassword === password) {
+    alert("Пароль введен верно");
+  } else {
+    alert("Пароль введен неправильно");
+  }
