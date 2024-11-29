5fc8aa70e7b59dc275ae7212a2c08b438b5abd1a
Olcik-resh <olechkaresetova@gmail.com>
2024-11-29 16:29:34 +0300

    Закончила верстку + JS

--- script.js
+++ script.js
@@ -5,3 +5,80 @@ if (userPassword === password) {
   } else {
     alert("Пароль введен неправильно");
   }
+
+
+//task2
+
+let c = 2;
+
+if (c > 0 && c < 10) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
+}
+
+//task3 
+
+let d = 150;
+let e = 25;
+
+if (d > 100 || e > 100) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
+}
+
+//task4 
+
+let a = '2';
+let b = '3';
+
+a = parseInt(a); //не нашла решения в уроке, загуглтла
+b = parseInt(b);
+
+alert(a + b);
+
+//task5
+
+let monthNumber = 12;
+
+switch (monthNumber) {
+  case 1:
+    console.log("Зима");
+    break;
+  case 2:
+    console.log("Зима");
+    break;
+  case 3:
+    console.log("Весна");
+    break;
+  case 4:
+    console.log("Весна");
+    break;
+  case 5:
+    console.log("Весна");
+    break;
+  case 6:
+    console.log("Лето");
+    break;
+  case 7:
+    console.log("Лето");
+    break;
+  case 8:
+    console.log("Осень");
+    break;
+  case 9:
+    console.log("Осень");
+    break;
+  case 10:
+    console.log("Осень");
+    break;
+  case 11:
+    console.log("Зима");
+    break;
+  default:
+    if (monthNumber > 13) {
+      console.log("Некорректный ввод данных");
+    }
+}
+
